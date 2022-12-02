ff53eda6122127ce79f5ef692849b294cac36cd7
Joseph Peters <petersjl@rose-hulman.edu>
2022-12-02 10:00:02 -0500

    Day 2 Part 1

---
+++ 02/Dart/Part1.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import '../../DartUtils.dart';
+
+void main(){
+  Stopwatch stopwatch = new Stopwatch()..start();
+  solvePuzzle();
+  print('Ran in ${stopwatch.elapsedMilliseconds * 1/1000} seconds');
+}
+
+Object parseInput([bool test = false]){
+  String filePath = Utils.to_abs_path(test ? '../testinput.txt' : '../input.txt');
+  Object input = File(filePath).readAsStringSync().splitNewLine().listMap<Pair<String,String>>((String line) {
+    var parts = line.split(' ');
+    return Pair<String,String>(parts[0], parts[1]);
+  });
+  return input;
+}
+
+// The main method of the puzzle solve
+void solvePuzzle(){
+  var moves = parseInput() as List<Pair<String,String>>;
+  int total = 0;
+  for(var play in moves) total += score(play);
+  print('This method results in a score of $total');
+}
+
+int score(Pair<String,String> play){
+  switch(play.first){
+    case 'A':
+      switch(play.second){
+        case 'X': return 1 + 3;
+        case 'Y': return 2 + 6;
+        case 'Z': return 3 + 0;
+        default: return 0;
+      }
+    case 'B':
+      switch(play.second){
+        case 'X': return 1 + 0;
+        case 'Y': return 2 + 3;
+        case 'Z': return 3 + 6;
+        default: return 0;
+      }
+    case 'C':
+      switch(play.second){
+        case 'X': return 1 + 6;
+        case 'Y': return 2 + 0;
+        case 'Z': return 3 + 3;
+        default: return 0;
+      }
+    default : return 0;
+  }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ The advent of code for 2022
 
 Calendar:
 - Day 1: Calorie Counting
+- Day 2: Rock Paper Scissors
