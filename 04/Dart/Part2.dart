648c4d6d578b1b2a907c7d2921a917376f785303
Joseph Peters <petersjl@rose-hulman.edu>
2022-12-04 21:49:54 -0500

    Day 4 Part 2

---
+++ 04/Dart/Part2.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import '../../DartUtils.dart';
+
+void main() {
+  Stopwatch stopwatch = new Stopwatch()..start();
+  solvePuzzle();
+  print('Ran in ${stopwatch.elapsedMilliseconds * 1 / 1000} seconds');
+}
+
+Object parseInput([bool test = false]) {
+  String filePath = Utils.to_abs_path(test ? '../testinput.txt' : '../input.txt');
+  var input = File(filePath)
+      .readAsStringSync()
+      .splitNewLine()
+      .listMap<Pair<Pair<int, int>, Pair<int, int>>>((String line) {
+    var parts = line.split(',');
+    var elfOne = parts[0].split('-').listMap<int>((thing) => int.parse(thing));
+    var elfTwo = parts[1].split('-').listMap<int>((thing) => int.parse(thing));
+    return Pair(Pair(elfOne[0], elfOne[1]), Pair(elfTwo[0], elfTwo[1]));
+  });
+  return input;
+}
+
+// The main method of the puzzle solve
+void solvePuzzle() {
+  var pairs = parseInput() as List<Pair<Pair<int, int>, Pair<int, int>>>;
+  int count = 0;
+  for (var pair in pairs) if (encapsulates(pair.first, pair.second)) count++;
+  print('Ranges that overlap: $count');
+}
+
+bool encapsulates(Pair<int, int> first, Pair<int, int> second) {
+  var smaller = first.first < second.first ? first : second;
+  var larger = first.second > second.second ? first : second;
+  return smaller == larger || smaller.second >= larger.first;
+}
